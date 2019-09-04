935fc12f2399aba8efd0ce1182dec3d1ac2339f0
angelcamach0 <54860242+angelcamach0@users.noreply.github.com>
2019-09-04 16:58:43 -0600

    Add files via upload

---
+++ calls.c
@@ -0,0 +1,49 @@
+//CS 271 - lab assignment #2
+// program name
+// purpose
+// by: angel camacho
+// sep 3, 2019
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+    double numCalls=0;
+    double mean=0;
+    double min=0;
+    double max=0;
+    int value_read; 
+    
+   scanf("%d", &value_read);
+    printf("%d This is the mean: ", value_read);
+//     while(value_read != -1){
+//         value_read = scanf("%d", &value_read);
+//        if(value_read != -1){
+//            mean = mean + value_read;
+//         printf("%7.5f This is the mean: ", value_read);
+//            numCalls++;
+//         
+//            keep count of min val
+//            if(min > value_read ){
+//                min = value_read;
+//            }
+//            keep count of max val
+//            if(max < value_read ){
+//                max = value_read;
+//            }
+//        }else{
+//        break; 
+//        }
+//      break;
+//     }
+    
+    // mean = mean/100;
+    
+    //output stuff
+   // printf("%7.5f This is the mean: ", mean);
+    
+}
+
+
+
+
+
